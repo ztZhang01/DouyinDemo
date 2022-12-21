84f2e1a4489a6023fb9180dd6712d6f8499bd214
ztZhang01 <121099882+ztZhang01@users.noreply.github.com>
2022-12-21 17:23:51 +0800

    Add files via upload

---
+++ DouyinDemo/DouyinDemo/DouyinDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DouyinDemo
+//
+//  Created by 张仲廷 on 2022/12/21.
+//
+
+import UIKit
+import Kingfisher
+import SnapKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
